13f55f2fd35f39609965cc226e46e9650a531259
danya61 <daniilspirit@gmail.com>
2016-07-11 15:51:53 +0300

    создал выбор : аккорды или табы

---
+++ GuitarHelper WatchKit Extension/CordsRowControl.swift
@@ -0,0 +1,14 @@
+//
+//  TableRowControl.swift
+//  GuitarHelper
+//
+//  Created by Danya on 11.07.16.
+//  Copyright © 2016 Ivan Pryadchenko. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class TableRowControl : WKInterfaceController{
+    
+}
