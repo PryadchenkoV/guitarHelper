9923dc81130f2077adabe9dd99026cc16deafa3f
Ivan Pryadchenko <lordpomedor@gmail.com>
2016-07-11 15:49:24 +0300

    Список песен

---
+++ GuitarHelper WatchKit Extension/MainScreenSongList.swift
@@ -0,0 +1,17 @@
+//
+//  MainScreenSongList.swift
+//  GuitarHelper
+//
+//  Created by Ivan Pryadchenko on 11.07.16.
+//  Copyright © 2016 Ivan Pryadchenko. All rights reserved.
+//
+
+import WatchKit
+
+class MainScreenSongList: NSObject {
+    
+    
+    @IBOutlet var lableSongText: WKInterfaceLabel!
+    
+
+}
