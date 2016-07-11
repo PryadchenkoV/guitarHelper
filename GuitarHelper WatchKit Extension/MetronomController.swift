e8619a2c8786a178ed99835fed814f988429e279
Ivan Pryadchenko <lordpomedor@gmail.com>
2016-07-11 19:13:09 +0300

    метроном + интрефейс

---
+++ GuitarHelper WatchKit Extension/MetronomController.swift
@@ -0,0 +1,13 @@
+//
+//  MetronomController.swift
+//  GuitarHelper
+//
+//  Created by Ivan Pryadchenko on 11.07.16.
+//  Copyright © 2016 Ivan Pryadchenko. All rights reserved.
+//
+
+import WatchKit
+
+class MetronomController: UIViewController {
+
+}
